15c7f2ff4ff385bfe948b239959ff64d9e6c9e23
Steven Buss <steven.buss@gmail.com>
2018-07-29 13:18:59 -0700

    Handle different media URIs

--- resource.go
+++ resource.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 type Resource struct {
@@ -14,26 +15,26 @@ type Resource struct {
 	Length uint64
 }
 
-func NewResource(uri string) (*Resource, error) {
+func asx(uri string) (string, error) {
 	u, err := url.ParseRequestURI(uri)
 	if err != nil {
-		return nil, fmt.Errorf("could not parse uri: %v", err)
+		return "", fmt.Errorf("could not parse uri: %v", err)
 	}
 	clip_id := u.Query().Get("clip_id")
 	if clip_id == "" {
-		return nil, fmt.Errorf("Could not find clip_id in URI '%s'", uri)
+		return "", fmt.Errorf("Could not find clip_id in URI '%s'", uri)
 	}
 
 	// Now we have clip_id, we can get the media URI
 	infoUri := fmt.Sprintf("http://%s/ASX.php?clip_id=%s", u.Host, clip_id)
 	resp, err := http.Get(infoUri)
 	if err != nil {
-		return nil, fmt.Errorf("couldn't fetch uri (%s): %v", infoUri, err)
+		return "", fmt.Errorf("couldn't fetch uri (%s): %v", infoUri, err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("couldn't read body of uri (%s): %v", infoUri, err)
+		return "", fmt.Errorf("couldn't read body of uri (%s): %v", infoUri, err)
 	}
 	// response looks like:
 	// <ASX version="3.0">
@@ -48,11 +49,38 @@ func NewResource(uri string) (*Resource, error) {
 	pattern := regexp.MustCompile(`OnDemand/mp4:(([^.]+)\.mp4).*`)
 	mediaId := pattern.FindStringSubmatch(string(body))[1]
 	if mediaId == "" {
-		return nil, fmt.Errorf("couldn't find mediaId: %s", body)
+		return "", fmt.Errorf("couldn't find mediaId: %s", body)
 	}
-	mediaUri := fmt.Sprintf("http://media-06.granicus.com:443/OnDemand/%s", mediaId)
+	mediaUri := fmt.Sprintf("http://archive-media.granicus.com:443/OnDemand/%s", mediaId)
+	return mediaUri, nil
+}
 
-	resp, err = http.DefaultClient.Head(mediaUri)
+// NewResource takes any of the common media urls and returns a Resource
+//
+// The common urls look like:
+//   * http://sanfrancisco.granicus.com/DownloadFile.php?view_id=10&clip_id=30229
+//     via the RSS feed http://sanfrancisco.granicus.com/ViewPublisherRSS.php?view_id=10
+//   * http://sanfrancisco.granicus.com/MediaPlayer.php?view_id=10&clip_id=31040
+//     via the Board/Commission page http://sanfrancisco.granicus.com/ViewPublisher.php?%20%20%20%20view_id=10
+//   * media-06.granicus.com:443/OnDemand/sanfrancisco/sanfrancisco_c6d8c565-1eba-41b8-b9c2-f5999a2b141f.mp4
+//     via some manual inspection of resources that the playback page fetches
+//   * http://archive-media.granicus.com:443/OnDemand/sanfrancisco/sanfrancisco_618361c0-0a52-4ab0-ae84-e707fc02bc2b.mp4
+//     via more manual inspection
+// All of these eventually get us to a url which looks like
+// http://archive-media.granicus.com:443/OnDemand/sanfrancisco/sanfrancisco_c6d8c565-1eba-41b8-b9c2-f5999a2b141f.mp4
+func NewResource(uri string) (*Resource, error) {
+	var mediaUri string
+	var err error
+	if strings.Contains(uri, "DownloadFile") || strings.Contains(uri, "MediaPlayer") {
+		mediaUri, err = asx(uri)
+		if err != nil {
+			return nil, fmt.Errorf("asx: %v", err)
+		}
+	} else if strings.Contains(uri, ".mp4") {
+		mediaUri = uri
+	}
+
+	resp, err := http.DefaultClient.Head(mediaUri)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't read uri (%s): %v", mediaUri, err)
 	}
